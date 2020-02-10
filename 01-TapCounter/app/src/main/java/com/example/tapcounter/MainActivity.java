bba7ef88fbab3dcadbde4b323c0e1881d6b5ab5c
mike <mpakleung@gmail.com>
2020-02-10 15:56:05 -0500

    Complete the Tapholder project
    Button long click and touch event listener

--- 01-TapCounter/app/src/main/java/com/example/tapcounter/MainActivity.java
+++ 01-TapCounter/app/src/main/java/com/example/tapcounter/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,6 +15,9 @@ public class MainActivity extends AppCompatActivity {
     private int counter;
     private TextView counterLabel;
 
+    long lastDown;
+    long keyPressedDuration ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
         // Label
         counterLabel = (TextView)findViewById(R.id.counterLabel);
-        initCounter();
-        incrementCounter();
+//        initCounter();
+//        incrementCounter();
 
 
         // Toolbar
@@ -33,28 +37,46 @@ public class MainActivity extends AppCompatActivity {
 
         Button button = (Button) findViewById(R.id.toolbar_overflow_menu_button);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                // Perform action on click
-                initCounter();
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                // Perform action on click
+//                initCounter();
+//            }
+//        });
+
+
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // start your timer
+                    lastDown = System.currentTimeMillis();
+
+//                    counterLabel.setText(String.valueOf(lastDown));
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // stop your timer.
+                    keyPressedDuration = System.currentTimeMillis() - lastDown;
+                }
+                return false;
             }
         });
 
     }
-
-    private void initCounter() {
-        counter = 0;
-        counterLabel.setText(String.valueOf(counter));
-    }
-
-    private void incrementCounter() {
-        // Button
-        final Button button = (Button) findViewById(R.id.tapButton);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                counter++;
-                counterLabel.setText(String.valueOf(counter));
-            }
-        });
-    }
+//
+//    private void initCounter() {
+//        counter = 0;
+//        counterLabel.setText(String.valueOf(counter));
+//    }
+//
+//    private void incrementCounter() {
+//        // Button
+//        final Button button = (Button) findViewById(R.id.tapButton);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                counter++;
+//                counterLabel.setText(String.valueOf(counter));
+//            }
+//        });
+//    }
 }
---
+++ 02-TapHolder/app/src/main/java/com/example/tapholder/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.tapholder;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private int counter;
+    private TextView counterLabel;
+    private Button tapButton;
+    private Button toolbarButton;
+    private Toolbar toolbar;
+
+    CountDownTimer yourCountDownTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        counter = 0;
+        // Label
+        counterLabel = (TextView)findViewById(R.id.counterLabel);
+
+        handleToolbar();
+
+        handleTapButton();
+
+
+
+    }
+
+    public void handleToolbar() {
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+        getSupportActionBar().setTitle("Tap Holder");
+
+        tapButton = (Button) findViewById(R.id.tapButton);
+        toolbarButton = (Button) findViewById(R.id.toolbar_overflow_menu_button);
+
+        toolbarButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                counter = 0;
+                counterLabel.setText(String.valueOf(counter));
+            }
+        });
+
+    }
+
+    public void handleTapButton() {
+        tapButton.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                    // start your timer
+
+                    yourCountDownTimer = new CountDownTimer(10000, 100){
+                        public void onTick(long millisUntilFinished){
+                            counterLabel.setText(String.valueOf(counter));
+                            counter++;
+                        }
+                        public  void onFinish(){
+                            //counterLabel.setText("FINISH!!");
+                        }
+                    }.start();
+
+                } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                    // stop your timer.
+                    yourCountDownTimer.cancel();
+                }
+                return false;
+            }
+        });
+
+    }
+
+}
