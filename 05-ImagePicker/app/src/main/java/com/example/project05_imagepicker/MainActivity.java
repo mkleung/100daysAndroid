8fc974d2fff370ddbfdf499bdfc0ab2344a1d2d9
mike <mpakleung@gmail.com>
2020-03-03 20:18:15 -0500

    Add load from gallery feature button

--- 05-ImagePicker/app/src/main/java/com/example/project05_imagepicker/MainActivity.java
+++ 05-ImagePicker/app/src/main/java/com/example/project05_imagepicker/MainActivity.java
@@ -8,6 +8,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -35,73 +36,82 @@ public class MainActivity<onActivityResult> extends AppCompatActivity {
     private static final String TAG = "MyActivity";
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private static final int PICK_IMAGE = 100;
+    Uri imageUri;
+
     private Bitmap mImageBitmap;
     private String mCurrentPhotoPath;
     private ImageView mImageView;
-    Button button;
+    Button btnCaptureImage;
+    Button galleryButton;
+
+    private File imageFile;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageView = findViewById(R.id.mImageView);
-        button = findViewById(R.id.btnCaptureImage);
+        mImageView = (ImageView) findViewById(R.id.mImageView);
+        btnCaptureImage = (Button) findViewById(R.id.btnCaptureImage);
+        galleryButton = (Button) findViewById(R.id.galleryButton);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        btnCaptureImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                if (cameraIntent.resolveActivity(getPackageManager()) != null) {
-                    // Create the File where the photo should go
-                    File photoFile = null;
-                    try {
-                        photoFile = createImageFile();
-                    } catch (IOException ex) {
-                        // Error occurred while creating the File
-                        Log.i(TAG, "IOException");
-                    }
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-                        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile));
-                        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE);
-                    }
-                }
+                imageFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "test.jpg");
 
+                // Store in image gallery
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(imageFile));
+
+                startActivityForResult(cameraIntent, 0);
+            }
+        });
+
+
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, PICK_IMAGE);
             }
         });
     }
 
 
-    private File createImageFile() throws IOException {
-        // Create an image file name
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File storageDir = Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(
-                imageFileName,  // prefix
-                ".jpg",         // suffix
-                storageDir      // directory
-        );
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
-        return image;
-    }
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            try {
-                mImageBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(mCurrentPhotoPath));
-                mImageView.setImageBitmap(mImageBitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        if (requestCode==0) {
+            switch (resultCode) {
+                case RESULT_OK:
+
+                    // This code makes image appear on gallery
+                    addImageToGallery(imageFile.getAbsolutePath(), getApplicationContext());
+
+                    // Display image on imageView
+                    File imgFile = new  File(imageFile.getAbsolutePath());
+                    if(imgFile.exists()){
+                        Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+                        ImageView mImageView = (ImageView) findViewById(R.id.mImageView);
+                        mImageView.setImageBitmap(myBitmap);
+                    }
+                    break;
+                default: break;
             }
         }
     }
-}
 
+    public static void addImageToGallery(final String filePath, final Context context) {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(MediaStore.MediaColumns.DATA, filePath);
+        context.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+    }
+}
 
