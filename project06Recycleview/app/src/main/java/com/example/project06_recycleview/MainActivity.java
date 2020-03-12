684ac969dcac474a6bd96d6a2e4ef58a2c9f0461
mike <mpakleung@gmail.com>
2020-03-12 01:32:13 -0400

    Add project06 - RecycleView

---
+++ project06Recycleview/app/src/main/java/com/example/project06_recycleview/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.project06_recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//https://www.youtube.com/watch?v=18VcnYN5_LM
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
